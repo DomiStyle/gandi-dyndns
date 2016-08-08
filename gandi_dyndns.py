7d1d906923115f38a174b8c2a3eaeea3be4b1d0e
DomiStyle <pdominik@live.at>
2016-08-08 20:07:05 +0200

    Script now takes new IP from CLI argument
    Allows script to be called from firewalls like pfSense. Since the
    firewall already knows the IP address there is no need for external
    providers to be involved.

--- gandi_dyndns.py
+++ gandi_dyndns.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python2
 
-import collections
 import json
-import random
-import re
-import time
-import urllib2
 import xmlrpclib
+import os.path
+import socket
 
 import logging as log
 log.basicConfig(format='%(asctime)-15s [%(levelname)s] %(message)s', level=log.DEBUG)
-
-# matches all IPv4 addresses, including invalid ones. we look for
-# multiple-provider agreement before returning an IP.
-IP_ADDRESS_REGEX = re.compile('\d{1,3}(?:\.\d{1,3}){3}')
 
 class GandiServerProxy(object):
   '''
@@ -59,83 +52,6 @@ class GandiServerProxy(object):
     # call the proxy's method with the modified arguments
     return getattr(self.proxy, method)(*key_args)
 
-def get_external_ip_from_url(url):
-  '''Get all the IP addresses found at a given URL.'''
-
-  # open the website, download its data, and return all IP strings found
-  # we want to respect some site's filtering on User-Agent.
-  data = urllib2.urlopen(url, timeout=10).read()
-  addys = IP_ADDRESS_REGEX.findall(data)
-  return addys
-
-def get_external_ip(attempts=100, threshold=3):
-  '''Return our current external IP address, or None if there was an error.'''
-
-  # load the list of IP address providers
-  providers = load_providers()
-
-  # we want several different providers to agree on the address, otherwise we
-  # need to keep trying to get agreement. this prevents picking up 'addresses'
-  # that are really just strings of four dot-delimited numbers.
-  ip_counts = collections.Counter()
-
-  # the providers we're round-robining from
-  current_providers = []
-
-  while attempts > 0:
-    # reduce our attempt count every time, to ensure we'll exit eventually
-    attempts -= 1
-
-    # randomly shuffle the providers list when it's empty so we can round-robin
-    # from all the providers. also reset the counts, since double-counting
-    # results from the same providers might result in false-positives.
-    if not current_providers:
-      current_providers = providers[:]
-      random.shuffle(current_providers)
-      ip_counts = collections.Counter()
-
-    # get the provider we'll try this time
-    provider = current_providers.pop()
-
-    try:
-      addys = get_external_ip_from_url(provider)
-
-      # add a single address to the counter randomly to help prevent false
-      # positives. we don't add all the found addresses to guard against adding
-      # multiple false positives for the same site. taking a single random
-      # address and then checking it against the other sites is safer. what are
-      # the chances that several sites will return the same false-positive
-      # number?
-      if addys:
-        ip = random.choice(addys)
-        ip_counts.update({ ip: 1 })
-        log.debug('Got IP from provider %s: %s', provider, ip)
-
-      # check for agreeing IP addresses, and return the first address that meets
-      # or exceeds the count threshold.
-      for ip, count in ip_counts.most_common():
-        if count < threshold:
-          break
-        return ip
-
-    except Exception as e:
-      log.warning('Error getting external IP address from %s: %s', provider, e)
-
-      # sleep a bit after errors, in case it's a general network error. if it
-      # is, hopefully this will give some time for the network to come back up.
-      time.sleep(0.1 + random.random() * 2)
-
-  log.warning('Failed to get an external IP address after %d attempts!', attempts)
-
-  # return None if no agreement could be reached
-  return None
-
-def load_providers():
-  '''Load the providers file as a de-duplicated and normalized list of URLs.'''
-  with open('providers.json') as f:
-    providers = json.load(f)['providers']
-  return list(set([p.strip() for p in providers]))
-
 def load_config():
   '''Load the config file from disk.'''
   with open('config.json') as f:
@@ -164,19 +80,7 @@ def check_config(conf):
 
   return True
 
-def test_providers():
-  '''Test all IP providers and log the IPs they return.'''
-
-  for provider in load_providers():
-    log.debug('IPs found at %s:', provider)
-
-    try:
-      for ip in get_external_ip_from_url(provider):
-        log.debug('  %s', ip)
-    except Exception as e:
-      log.warning('Error getting external IP address from %s: %s', provider, e)
-
-def update_ip():
+def update_ip(external_ip):
   '''
   Check our external IP address and update Gandi's A-record to point to it if
   it has changed.
@@ -192,16 +96,7 @@ def update_ip():
   # create a connection to the Gandi production API
   gandi = GandiServerProxy(config['api_key'])
 
-  # see if the record's IP differs from ours
-  log.debug('Getting external IP...')
-  external_ip = get_external_ip()
-
-  log.debug('External IP is: %s', external_ip)
-
-  # make sure we actually got the external IP
-  if external_ip is None:
-    log.fatal('Could not get external IP.')
-    sys.exit(2)
+  log.debug('Updating dynamic IP: %s', external_ip)
 
   exit_code = 0
 
@@ -307,7 +202,7 @@ def update_ip():
     # set the new zone version as the active version
     log.info('Updating active zone version...')
     gandi.domain.zone.version.set(zone_id, new_version_id)
-
+    
     log.info('Set zone %d as the active zone version.', new_version_id)
     log.info('Dynamic record successfully updated to %s!', external_ip)
 
@@ -315,11 +210,20 @@ def update_ip():
     sys.exit(exit_code)
 
 def main(args):
-  # test all providers if specified, otherwise update the IP
-  if args[-1] == 'test':
-    test_providers()
+  if not os.path.isfile('config.json'):
+    log.info('No config.json found! Please rename config-example.json and adjust the file accordingly.')
+    sys.exit(1)
+
+  if len(args) > 1:
+    try:
+      socket.inet_aton(args[1])
+      update_ip(args[1])
+    except socket.error:
+      log.info('No valid IP given!')
+      sys.exit(1)
   else:
-    update_ip()
+    log.info('No IP given as argument!')
+    sys.exit(1)
 
 if __name__ == '__main__':
   import sys
